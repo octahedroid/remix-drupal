1c3f66ed04ceca39050595394b352011ce7d2519
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2022-11-14 13:37:54 -0800

    feat: update client ver to 0.1.4 (#1)

--- bindings.d.ts
+++ bindings.d.ts
@@ -2,7 +2,8 @@ export {}
 
 // ENV values from wrangler.toml
 declare global {
-  const DRUPAL_URI: string;
+  const DRUPAL_GRAPHQL_URI: string;
+  const DRUPAL_AUTH_URI: string;
   const DRUPAL_CLIENT_ID: string;
   const DRUPAL_CLIENT_SECRET: string;
 
