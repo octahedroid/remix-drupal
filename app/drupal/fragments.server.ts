e57194f9ced9e4b7f2a61fdfb027050060ad2986
Jesus Manuel Olivas (jmolivas) <jesus.olivas@gmail.com>
2023-02-22 12:04:21 -0800

    ✨ provide typing on fragments

--- app/drupal/fragments.server.ts
+++ app/drupal/fragments.server.ts
@@ -1,74 +1,77 @@
-const LinkFragment = {
+import type {
+  ImageGenqlSelection,
+  ImageStyleGenqlSelection,
+  LinkGenqlSelection,
+  MediaImageGenqlSelection,
+  ParagraphCodeBlockGenqlSelection,
+  ParagraphHeroCtaGenqlSelection,
+  ParagraphHeroTextGenqlSelection,
+  ParagraphImageGenqlSelection,
+  ParagraphTextGenqlSelection,
+  TextGenqlSelection
+} from '~/@types/gen';
+
+const LinkFragment: LinkGenqlSelection = {
   uri: true,
   link: true,
-  title: true
+  title: true,
 }
 
-const TextFragment = {
+const TextFragment: TextGenqlSelection = {
   format: true,
   value: true,
-  processed: true
+  processed: true,
 }
 
-const ImageFragment = {
-  url: true,
-  width: true,
-  height: true
-}
-
-const ImageStyleFragment = {
+const ImageStyleFragment: ImageStyleGenqlSelection = {
   url: true,
   width: true,
   height: true,
   style: true
 }
 
-export const MediaImageFragment = {
-  mediaImage: {
-    ...ImageFragment,
-    styles: {
-      ...ImageStyleFragment
-    }
-  }
+const ImageFragment: ImageGenqlSelection = {
+  url: true,
+  width: true,
+  height: true,
+  styles: ImageStyleFragment,
 }
 
-export const ParagraphHeroCtaFragment = {
+export const MediaImageFragment: MediaImageGenqlSelection = {
+  mediaImage: ImageFragment
+}
+
+export const ParagraphHeroCtaFragment: ParagraphHeroCtaGenqlSelection = {
   id: true,
   status: true,
   created: true,
-  cta: {
-    ...LinkFragment
-  },
+  cta: LinkFragment,
   intro: true,
   text: true,
   title: true
 }
 
-export const ParagraphTextFragment = {
+export const ParagraphTextFragment: ParagraphTextGenqlSelection = {
   id: true,
   status: true,
   created: true,
-  textRich: {
-    ...TextFragment
-  },
+  textRich: TextFragment,
   title: true
 }
 
-export const ParagraphCodeBlockFragment = {
+export const ParagraphCodeBlockFragment: ParagraphCodeBlockGenqlSelection = {
   title: true,
   code: true,
   language: true,
   showLineNumbers: true
 }
 
-export const ParagraphHeroTextFragment = {
+export const ParagraphHeroTextFragment: ParagraphHeroTextGenqlSelection = {
   intro: true,
   text: true,
-  title: true
+  title: true,
 }
 
-export const ParagraphImageFragment = {
-  image: {
-    ...MediaImageFragment
-  }
+export const ParagraphImageFragment: ParagraphImageGenqlSelection = {
+  image: MediaImageFragment,
 }
